74332bf6a70ce39e3addb50a435298cc2bdbb1af
hyunjung <space90220@gmail.com>
2024-03-20 23:51:09 +0900

    65강 ~ 73강

--- src/main/java/com/in28minutes/springboot/learnspringboot/LearnSpringBootApplication.java
+++ src/main/java/com/in28minutes/springboot/learnspringboot/LearnSpringBootApplication.java
@@ -3,7 +3,7 @@ package com.in28minutes.springboot.learnspringboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class LearnSpringBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(LearnSpringBootApplication.class, args);
