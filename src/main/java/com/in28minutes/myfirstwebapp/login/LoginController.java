568cf6a90414ac998c874ac11563de95acae9a1f
hyunjung <space90220@gmail.com>
2024-03-22 21:12:34 +0900

    86강 ~ 89강

---
+++ src/main/java/com/in28minutes/myfirstwebapp/login/LoginController.java
@@ -0,0 +1,25 @@
+package com.in28minutes.myfirstwebapp.login;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("login")
+    public String login(@RequestParam String name, ModelMap model)
+    {
+        model.put("name", name);
+        logger.debug("Request param is {}", name);
+        logger.info("I want this printed at info level");
+        logger.warn("I want this printed at warn level");
+        System.out.println("Request Param is "+name); // NOT RECOMMENDED FOR PROD CODE
+        return "login";
+    }
+}
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Login Page</title>
+</head>
+<body>
+    Welcome to toe login page ${name}!
+</body>
+</html>
